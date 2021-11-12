ddecb6307cdfa72eeb05bd861da49146d49d2cb1
Lowell Alleman <lowell@kintyre.co>
2021-11-12 18:17:26 -0500

    First implementation of 'parsespl'

--- bin/parse_spl.py
+++ bin/parse_spl.py
@@ -7,77 +7,37 @@ __version__ = "0.1.0"
 
 import os
 import sys
-import re
 import json
-import time
+import functools
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))  # nopep8
-
-import six
-import requests
-from cypresspoint.datatype import as_bool
-from cypresspoint.searchcommand import ensure_fields
-from cypresspoint.spath import splunk_dot_notation
-from requests.auth import HTTPBasicAuth
 from splunklib.searchcommands import dispatch, EventingCommand, Configuration, Option, validators
 
-
-""" http debug logging
-import logging
-from http.client import HTTPConnection  # py3
-
-log = logging.getLogger('urllib3')
-log.setLevel(logging.DEBUG)
-"""
+from splunklib.client import HTTPError
 
 
 @Configuration()
 class parsespl(EventingCommand):
-    """
 
-    ##Syntax
-
-    .. code-block::
-        parsespl entity=my_entity field=input
-
-    ##Description
-
-    ##Example
-    """
-    entity = Option(
+    mode = Option(
         require=False,
-        default="kintyre",
-        validate=validators.Match("entity", "[a-zA-Z0-9._]+"))
+        default="search",
+        validate=validators.Set("search", "raw", "eval", "where"))
 
-    output = Option(
-        require=True,
+    field = Option(
+        require=False,
         validate=validators.Fieldname())
 
-    field_set = Option(
-        require=False,
-        validate=validators.Set("a", "b", "c"))
+    value = Option(
+        require=False)
 
-    field_int = Option(
-        require=False,
-        default=32,
-        validate=validators.Integer(1, 128))
-
-    field_bool = Option(
-        require=False,
-        default=True,
-        validate=validators.Boolean())
-
-    """ COOKIECUTTER-TODO:  Use or delete these tips'n'tricks
-
-    *** Class-level stuff ***
-
-    # Always run on the searchhead (not the indexers)
+    # Run on SH only (do not distributed to indexers)
     distributed = False
 
     # Don't allow this to run in preview mode to limit API hits
     run_in_preview = False
 
-
+    """
     *** Method-level stuff ***
 
     # Log the commands given to the SPL command:
@@ -85,7 +45,6 @@ class parsespl(EventingCommand):
 
     # Access metadata about the search, such as earliest_time for the selected time range
     self.metadata.searchinfo.earliest_time
-
 
     *** Runtime / testing ***
 
@@ -95,67 +54,93 @@ class parsespl(EventingCommand):
 
     """
 
-    def __init__(self):
-        super(parsespl, self).__init__()
-        # COOKIECUTTER-TODO: initialize any custom variables in __init__()
-
     def prepare(self):
         super(parsespl, self).prepare()
-        # COOKIECUTTER-TODO: Customize or DELETE prepare() - arg validation & REST/CONF fetch
-
         will_execute = bool(self.metadata.searchinfo.sid and
                             not self.metadata.searchinfo.sid.startswith("searchparsetmp_"))
+        '''
         if will_execute:
             self.logger.info("Launching version %s", __version__)
+        '''
 
-        ''' COOKIECUTTER-TODO: Enable/delete: this block of code will prevent unused/unknown paramaters
-        # Check to see if an unused arguments remain after argument parsing
         if self.fieldnames:
             self.write_error("The following arguments to parsespl are "
                              "unknown:  {!r}  Please check the syntax.", self.fieldnames)
             sys.exit(1)
-        '''
-        # COOKIECUTTER-TODO:  Implement argument validation here, if needed
 
-        if not will_execute:
-            return
-        # COOKIECUTTER-TODO:  Add custom REST endpoint/conf snippet here, if needed
+        # Ensure that we either have 'field' or 'value' but not both:
+        have_field = bool(self.field)
+        have_value = bool(self.value)
 
-    def _query_external_api(self, query_string):
-        # COOKIECUTTER-TODO: Implement remote Kintyre ES Post Filter API query here
-        """ Handle the query to Kintyre API that drives this SPL command
-        Returns (error, payload)
-        """
-        query_params = {
-            "search": query_string
-        }
-        headers = {
-            'content-type': "application/json",
-            'x-api-auth': self.api_auth,
-            'cache-control': "no-cache"
-        }
+        if not have_field ^ have_value:
+            self.write_error("Please provide either a reference 'field' or an "
+                             "expresion in 'value', but not both")
+            sys.exit(1)
+
+        if not have_field:
+            self.write_error("Only field is currently implemented.")
+            sys.exit(1)
+            # In value mode, we switch into a 'generating' commands not a eventing command
+
+    @functools.lru_cache(1000)
+    def parse_search(self, query):
+        data = error = None
         try:
-            response = requests.request("GET", self.api_url, headers=headers, params=query_params)
-        except requests.ConnectionError as e:
-            self.logger.error("Aborting due to API connection failure.  %s", e)
-            return ("Kintyre ES Post Filter Connection failure:  {}".format(e), [])
-        except Exception:
-            self.logger.exception("Failure while calling API")
-            return ("Kintyre ES Post Filter API Call failed", [])
-        result = response.json()
-        if isinstance(result, dict) and "message" in result:
-            return ("API returned message:  {}".format(result["message"]), result)
-        elif len(result) == 0:
-            result = []
-        return (None, result)
+            response = self.service.get("search/parser", q=query, output_mode="json")
+            data = json.load(response.body)
+        except HTTPError as e:
+            try:
+                data = json.loads(e.body)
+                error = [m["text"] for m in data["messages"]]
+            except (ValueError, TypeError):
+                error = [str(e)]
+        if error:
+            return error, data
+        else:
+            return "", data
+
+    @staticmethod
+    def normalize_search(spl):
+        if not spl.strip().startswith("|"):
+            spl = "search " + spl
+        return spl
 
     def transform(self, records):
-        # COOKIECUTTER-TODO: Replace this code with your own transforming logic
-        """ Transforming function that processes existing events and yields modified records to the Splunk events pipeline.
-        """
-        l = list(records)
-        l.sort(key=lambda r: r['_raw'])
-        return l
+        mode = self.mode
+        field = self.field
+
+        if mode == "search":
+            translate = self.normalize_search
+        elif mode == "eval":
+            def translate(s): return "makeresults | eval {}".format(s)
+        elif mode == "where":
+            def translate(s): return "makeresults | eval cond = if({}, 0, 1)".format(s)
+        else:
+            def translate(s): return s
+
+        for record in records:
+            query = record.get(field, None)
+            if query:
+                expr = translate(query)
+                '''
+                if expr != query:
+                    self.logger.info("Translated %r to %r", query, expr)
+                else:
+                    self.logger.info("[%s] Keeping %r", mode, expr)
+                '''
+                try:
+                    status, info = self.parse_search(expr)
+                    if info and isinstance(info, dict):
+                        info = json.dumps(info, indent=2)
+                    record["error"], record["info"] = status, info
+                except Exception as e:
+                    self.logger.exception("Error while handling %s expression:  %s", mode, expr)
+                    raise
+            else:
+                # Silently ignore any records without 'field'; keep output field placeholder
+                record["status"] = record["info"] = None
+
+            yield record
 
 
 if __name__ == '__main__':
